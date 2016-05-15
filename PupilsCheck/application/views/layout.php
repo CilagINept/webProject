e10e7e6576fdf753e0cc7d1071d25aa099cb8fe8
glickel <lickel.gaetan@gmail.com>
2016-05-15 12:05:50 +0200

    Auth + assets
    Ajout du module authentification auth_ion + modfication src et href assets

--- PupilsCheck/application/views/layout.php
+++ PupilsCheck/application/views/layout.php
@@ -5,8 +5,8 @@
 <html lang="fr">
     <head>
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link type="text/css" rel="stylesheet" href="public/css/materialize.css"  media="screen,projection"/>
-        <link type="text/css" rel="stylesheet" href="public/css/pupilscheck.css">
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url('public/css/materialize.css'); ?>"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url('public/css/pupilscheck.css'); ?>">
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta charset="utf-8"/>
@@ -44,15 +44,6 @@
                         <h5 class="white-text">Footer Content</h5>
                         <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
                     </div>
-                    <!--<div class="col l4 offset-l2 s12">
-                        <h5 class="white-text">Links</h5>
-                        <ul>
-                            <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                        </ul>
-                    </div>-->
                 </div>
             </div>
             <div class="footer-copyright">
@@ -63,8 +54,8 @@
             </div>
         </footer>
 
-        <script type="application/javascript" src="public/js/jquery-1.12.3.js"></script>
-        <script type="application/javascript" src="public/js/materialize.js"></script>
-        <script type="application/javascript" src="public/js/pupilscheck.js"></script>
+        <script type="application/javascript" src="<?php echo base_url().'public/js/jquery-1.12.3.js' ?>"></script>
+        <script type="application/javascript" src="<?php echo base_url().'public/js/materialize.js' ?>"></script>
+        <script type="application/javascript" src="<?php echo base_url().'public/js/pupilscheck.js' ?>"></script>
     </body>
 </html>
